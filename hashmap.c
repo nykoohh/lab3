fa046c73722fc1e447cd92db958be6aa560e5c15
Your Name <you@example.com>
2026-09-13 23:20:52 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -228,6 +228,17 @@ void enlarge(HashMap * map)
     }
     map -> size = 0;
 
+    for (long i = 0; i < old_capacity; i++)
+    {
+        if (old_buckets[i] != NULL)
+        {
+            if (old_buckets[i] -> key != NULL)
+            {
+                insertMap(map, old_buckets[i] -> key, old_buckets[i] -> value);
+            }
+        }
+    }
+
 }
 
 
